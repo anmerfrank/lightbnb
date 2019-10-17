136c065c80d96075d8c546df12b6a84a47754338
Anne Franklin <anmerfrank@gmail.com>
2019-10-17 20:43:34 +0000

    Added 'All My Reservations' feature via SQL

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -70,8 +70,14 @@ exports.addUser = addUser;
  */
 const getAllReservations = function(guest_id, limit = 10) {
   return pool.query(`
-  SELECT * FROM reservations
-  WHERE user = ${guest_id}
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = ${guest_id}
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
   LIMIT $1`
   , [limit])
   .then(res => res.rows);
